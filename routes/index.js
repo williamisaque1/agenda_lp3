66601715dd52c858dad68f3eb774d3386c491823
william isaque <williamisaque18@gmail.com>
2020-06-03 12:32:48 -0300

    atualizado

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const { Router } = require("express");
 
 const tipoContatoRouter = require("./tipoContato.routes");
-const participantesEventos = require("./participantesEvento.routes");
 const local = require("./Local.routes");
 
 const routes = Router();
 
 routes.use("/tipoContato", tipoContatoRouter);
-routes.use("/participantesEventos",participantesEventos);
 routes.use("/local",local);
 module.exports = routes;
 
