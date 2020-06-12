4c635d86710e839f7cba8402f9a147be83003052
william isaque <williamisaque18@gmail.com>
2020-06-12 15:08:40 -0300

    controler evento

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 
 const tipoContatoRouter = require("./tipoContato.routes");
 const localRouter = require("./Local.routes");
-
+const evento = require('./Local.routes');
 const routes = Router();
 
 routes.use("/tipoContato", tipoContatoRouter);
 routes.use("/local",localRouter);
+routes.use('/evento',evento);
 module.exports = routes;
 
