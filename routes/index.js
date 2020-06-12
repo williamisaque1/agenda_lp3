cddbe5c33f04c082c83b6ee63a4b3c292a85336c
william isaque <williamisaque18@gmail.com>
2020-06-12 20:53:06 -0300

    controller atualizado,

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const tipoContatoRouter = require("./tipoContato.routes");
 const localRouter = require("./Local.routes");
-const eventoRouter = require("../controlles/eventoController");
+const eventoRouter = require("./evento.routes");
 const routes = Router();
 console.log("codigo chegou aqui2");
 routes.use("/tipoContato", tipoContatoRouter);
