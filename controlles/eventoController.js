df296f59f8552b1f08e04fdbb69410070250c90a
william isaque <williamisaque18@gmail.com>
2020-06-12 21:10:36 -0300

    controller atualizado,

--- controlles/eventoController.js
+++ controlles/eventoController.js
@@ -1,13 +1,13 @@
 const { Client } = require("pg");
 class eventoController {
-    async index(){
+    async index() {
         try {
-            console.log("codigo chegou aqui");
+
             const client = new Client({
                 connectionString: process.env.DATABASE_URL,
                 ssl: {
                     rejectionUnauthorized: false,
-                   
+
 
                 },
             });
@@ -17,12 +17,17 @@ class eventoController {
             console.log("codigo chegou aqui3");
             client.end();
             const results = result.rows;
-            return results;
+            return {results};
+              
         } catch (err) {
             console.error(err);
-            return response.json(err);  
-        }
+            return response.json(err);
+}
+        
+
 
     }
+
 }
+
 module.exports = eventoController;
