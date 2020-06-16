be9541b69835d81feaeba02a2970e4220fa57336
william isaque <williamisaque18@gmail.com>
2020-06-16 15:09:41 -0300

    novaoo

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const routes = require("./routes/index");
-
+const cors = require("cors");
 const app = express();
+app.use(
+    cors({origin:"",})
+    );
+
 app.use(express.json());
 app.use(routes);
 
