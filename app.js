1fe89d70733c030950058e87e3bb126a89eb2605
william isaque <williamisaque18@gmail.com>
2020-06-16 17:27:54 -0300

    agora vai

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const routes = require("./routes/index");
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
-app.use(
+/*app.use(
     cors({origin:"",})
-    );
+    );*/
 
 app.use(express.json());
 app.use(routes);
