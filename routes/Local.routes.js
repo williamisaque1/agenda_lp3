c1d2fd0d25a12a27c02c034bd9420f0688a79798
william isaque <williamisaque18@gmail.com>
2020-06-20 23:25:01 -0300

    todos controllers concluidos utimate

--- routes/Local.routes.js
+++ routes/Local.routes.js
@@ -4,17 +4,17 @@ const localRouter = Router();
 const localcontroller = new Localcontroller();
 
 localRouter.get("/",async (request,response) =>{
- const items = localcontroller.index();
+ const items = await localcontroller.index();
   return response.json(items);
 });
 localRouter.post("/",async(request,response) => {
   const {cep, endereco, numero, bairro, complemento, cidade, estado} = request.body;
-  const items = localcontroller.create(cep, endereco, numero, bairro, complemento, cidade, estado);
+  const items = await localcontroller.create(cep, endereco, numero, bairro, complemento, cidade, estado);
 return response.json(items);
 });
 localRouter.delete("/:id",async(request,response) => {
   const {id} = request.params; 
-  const items = localcontroller.delete(id);
+  const items =  await localcontroller.delete(id);
   return response.json(items);
 });
 
