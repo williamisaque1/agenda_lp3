9b489236d4da19950df63487592941b81e24ecbb
william isaque <williamisaque18@gmail.com>
2020-06-21 10:31:08 -0300

    cors liberado

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const routes = require("./routes/index");
-//const cors = require("cors");
+const cors = require("cors");
 const app = express();
-app.use(
-    cors()
-    );
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
