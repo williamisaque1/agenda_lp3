4e59e595f6445e330e492f991555f2a79e4816c5
william isaque <williamisaque18@gmail.com>
2020-06-20 22:05:18 -0300

    todos controllers concluidos utimate

--- routes/Local.routes.js
+++ routes/Local.routes.js
@@ -1,17 +1,23 @@
-const {Router} = require("express");
-const localcontroller = require("../controlles/localController");
+const { Router } = require("express");
+const localController = require("../controlles/localController");
+const { request, response } = require("../app");
+const localRouter = Router();
+const localcontroller = new localController();
+
+localRouter.get("/",async (request,response) =>{
+ const items = localcontroller.index();
+  return response.json(items);
+});
+localRouter.post("/",async(request,response) => {
+  const {cep, endereco, numero, bairro, complemento, cidade, estado} = request.body;
+  const items = localcontroller.create(cep, endereco, numero, bairro, complemento, cidade, estado);
+return response.json(items);
+});
+localRouter.delete("/:id",async(request,response) => {
+  const {id} = request.params; 
+  const items = localcontroller.delete(id);
+  return response.json(items);
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = LocalRouter;
+module.exports = localRouter;
